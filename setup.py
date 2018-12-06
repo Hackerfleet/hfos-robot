119c6e40bfef7c6799295e52723f63cee17d1732
ri0t <riot@c-base.org>
2018-12-06 21:43:44 +0100

    Final content split HFOS -> Isomer

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ This software package is a plugin module for HFOS.
 """,
       dependency_links=[],
       install_requires=[
-          'hfos>=1.2.0',
+          'isomer>=1.0.0',
           'pyserial>=3.1.1'
       ],
       entry_points="""[isomer.components]
